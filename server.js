07885afafc71c9cab4ecdbb8ae04f57b670f0c6a
Rahul Super <rahul@truminds.co.in>
2020-05-13 04:26:54 +0530

    changes

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const bodyParser = require("body-parser");
-
+const cors = require("cors");
 // create express app
 const app = express();
+
+var corsOptions = {
+    origin: "*"
+};
+
+app.use(cors(corsOptions));
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
