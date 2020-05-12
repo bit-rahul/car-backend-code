e025e98664dddfe7faaa353277dd6abab92e8c83
Rahul Super <rahul@truminds.co.in>
2020-05-12 15:53:53 +0530

    changes

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require("body-parser");
 
-app.use(express.static('public'))
+// create express app
+const app = express();
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json())
+
+// define a simple route
+app.get('/', (req, res) => {
+    res.json({"message": "Welcome to RentVroom Car Booking Web!!"});
+});
+
+// app.use(express.static('public'))
 
 app.listen(process.env.PORT || 8080, () => console.log("ALL OK!!!"))
