d3d6f288c8f3704fa6d940dfbadc51367153e670
Rahul Super <rahul@truminds.co.in>
2020-05-12 15:03:26 +0530

    changes

--- server.js
+++ server.js
@@ -1,1 +1,6 @@
-console.log("HIIII")
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.listen(process.env.PORT || 8080, () => console.log("ALL OK!!!"))
